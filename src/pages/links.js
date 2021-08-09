759909e9529435401a9cac8aee65b24e5777da7d
michaelc <dylanesque27@gmail.com>
2021-08-09 12:00:47 -0400

    Retooled band dir structure

---
+++ src/pages/links.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Links() {
+    return (
+        <>
+        <h2>Other Metal Resources</h2>
+        <h2>Contact Us</h2>
+            </>
+    )
+}
